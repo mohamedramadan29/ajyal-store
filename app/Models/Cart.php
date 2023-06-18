735ef4e501623d7f37cc37ddb6fd01e165df47ba
mohamed <mr319242@gmail.com>
2023-06-18 08:10:00 +0300

    new update

---
+++ app/Models/Cart.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class Cart extends Model
+{
+    use HasFactory;
+    public $incrementing = false;
+    protected $fillable = [
+        'id', 'cookie_id', 'user_id', 'product_id', 'quantity', 'options'
+    ];
+
+    /**
+     * Events (Observed)
+     * creating , created , updating , updated , deleting , deleted,
+     * restoring , restored , retrived
+     */
+    // دي معناها وانت بتنشا الكارت يامعلم بدل ما تروح كل شي شوية تضيفها من الكونرولار لا ضيفها معاك مباشر من هنا 
+
+    public static function booted()
+    {
+        static::creating(function (Cart $cart ) {
+            $cart->id = Str::uuid();
+        });
+    }
+}
